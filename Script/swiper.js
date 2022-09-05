45e6ef24f4d514945350f097306c8aaf479ac7b1
Wilber Millan <yilbernet@gmail.com>
2022-09-05 01:08:53 -0500

    maquetado de secciones

---
+++ Script/swiper.js
@@ -0,0 +1,65 @@
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'vertical',
+   loop: true,
+ 
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ 
+   // And if we need scrollbar
+   scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+ });
+
+
+
+// const swiper = new Swiper('.swiper', {
+//   freeMode: true,
+// });
+
+// const swiper = new Swiper('.swiper', {
+//   freeMode: {
+//     enabled: true,
+//     sticky: true,
+//   },
+// });
+
+
+
+//  var swiper = new Swiper(".Swiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+/*var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});*/

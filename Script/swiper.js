90679945b5b53940797f25b6b1d76c57c4fb46cb
Wilber Millan <yilbernet@gmail.com>
2022-09-05 23:16:30 -0500

    maquetado de secciones

--- Script/swiper.js
+++ Script/swiper.js
@@ -1,68 +1,13 @@
 
 const swiper = new Swiper('.swiper', {
    // Optional parameters
-   slidesPerView: 1,
-   spaceBetween: 10,
-  //  direction: 'vertical',
-   loop: true,
- 
-   // If we need pagination
-   pagination: {
-     el: '.swiper-pagination',
-     clickable: true,
-   },
+   centeredSlides: true,
+   slidesPerView: 3,
+   spaceBetween: 5,
  
    // Navigation arrows
    navigation: {
      nextEl: '.swiper-button-next',
      prevEl: '.swiper-button-prev',
    },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
  });
-
-
-
-// const swiper = new Swiper('.swiper', {
-//   freeMode: true,
-// });
-
-// const swiper = new Swiper('.swiper', {
-//   freeMode: {
-//     enabled: true,
-//     sticky: true,
-//   },
-// });
-
-
-
-//  var swiper = new Swiper(".Swiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
-/*var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});*/
